d330dca1ed739fb79bd707f935c7563cc10ea1ad
Ben Madsen <benjamin.matheron@hotmail.fr>
2023-05-01 11:34:16 +0200

    J'avais oublié de save les scripts

--- _Jam04-28/Assets/Scripts/Components/ProjectileComponent.cs
+++ _Jam04-28/Assets/Scripts/Components/ProjectileComponent.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ProjectileComponent : MonoBehaviour
 {
     public float moveSpeed;
     public int damage;
     public float lifespan = 10f;
+    VFX_Handler vfx;
+
+    private void Awake()
+    {
+        vfx = GetComponent<VFX_Handler>();
+    }
 
     private void Start()
     {
@@ -21,6 +28,8 @@ public class ProjectileComponent : MonoBehaviour
     {
         if (col.collider.CompareTag("Enemy"))
         {
+            vfx.PlayAt(transform.position);
+
             col.gameObject.GetComponent<MeleeEnemyBehavior>().TakeDamage(damage);
             Destroy(this.gameObject);
         }
